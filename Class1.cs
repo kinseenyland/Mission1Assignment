0b23505774b5ab32142e71fd205dfaab929292a8
kinseenyland <kinseenyland@gmail.com>
2024-01-17 22:04:38 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission1Assignment
+{
+    public class DiceSimulator
+    {
+        private List<int> results;
+
+        public DiceSimulator()
+        {
+            results = new List<int>(new int[13]); // Indices 2 to 12 represent possible dice combinations
+        }
+
+        public List<int> SimulateRolls(int numberOfRolls)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                int sum = dice1 + dice2;
+
+                results[sum]++;
+            }
+
+            return results;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using Mission1Assignment;
+
+public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!");
+
+        Console.Write("How many dice rolls would you like to simulate? ");
+        int numberOfRolls = int.Parse(Console.ReadLine());
+
+        DiceSimulator diceSimulator = new DiceSimulator();
+        List<int> results = diceSimulator.SimulateRolls(numberOfRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+
+        int totalRolls = 0;
+        foreach (var result in results)
+        {
+            totalRolls += result;
+        }
+
+        Console.WriteLine($"Total number of rolls = {totalRolls}.");
+
+        for (int i = 2; i <= 12; i++)
+        {
+            int percentage = results[i] * 100 / totalRolls;
+            int asterisksCount = (percentage * numberOfRolls) / 100;
+            string histogram = new string('*', asterisksCount);
+            Console.WriteLine($"{i}: {histogram}");
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+    }
+}
